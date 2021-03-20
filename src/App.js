e24ffbca9475f10d59570998ce39ec6d33bfbed4
Tarekul Islam <probolbd@yahoo.com>
2021-03-20 15:59:36 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCoffee,
+  faAirFreshener,
+  faCheckSquare,
+} from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="btn btn-danger">
+        <FontAwesomeIcon icon={faCoffee} />
+        Danger
+      </button>
+      <br />
+      <br />
+      <button className="btn btn-secondary">
+        <FontAwesomeIcon icon={faCheckSquare} />
+        Danger
+      </button>
     </div>
   );
 }
